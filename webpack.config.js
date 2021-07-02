c7ba9676e1e96b2e85a3afceae4d76d031460882
mathletedev <mathletedev@gmail.com>
2021-07-02 12:26:54 -0700

    watch if env

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 const path = require("path");
 
-module.exports = {
-	entry: "./src/index.ts",
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: "ts-loader",
-				exclude: /node_modules/
-			}
-		]
-	},
-	resolve: {
-		extensions: [".tsx", ".ts", ".js"]
-	},
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "public/dist")
-	},
-	stats: "errors-only"
+module.exports = (env) => {
+	return {
+		entry: "./src/index.ts",
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: "ts-loader",
+					exclude: /node_modules/
+				}
+			]
+		},
+		resolve: {
+			extensions: [".tsx", ".ts", ".js"]
+		},
+		output: {
+			filename: "bundle.js",
+			path: path.resolve(__dirname, "public/dist")
+		},
+		watch: env.watch,
+		stats: "errors-only"
+	};
 };
